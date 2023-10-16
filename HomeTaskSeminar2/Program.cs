78d5537208598a8eb3f30d85a82a1c0221c5d850
Shloma Sergey <shlomasn@gmail.com>
2023-10-16 21:27:04 +0300

    Записал решение трех домашних задач в HomeTaskSeminar2

--- HomeTaskSeminar2/Program.cs
+++ HomeTaskSeminar2/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Вторая цифра числа: {number % 100 / 10}");
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number < 100)
+// {
+//     Console.Write("Третьей цифры нет");
+// }
+// else
+// {
+//     while(number > 1000)
+//     {
+//         number /= 10;
+//     }
+//     Console.Write(number % 10);
+// }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 5)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
