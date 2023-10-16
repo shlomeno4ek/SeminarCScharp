7ed015f4ccb9a2611271ff0ab190a483898d894d
Shloma Sergey <shlomasn@gmail.com>
2023-10-16 20:49:39 +0300

    Добавили три задачи в Seminar2

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 3
+// Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+//то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a % b == 0)
+// {
+//     Console.WriteLine("Кратно");
+// }
+// else
+// {
+//     Console.WriteLine($"Не кратно, остаток {a % b}");
+// }
+
+//Задача 2
+// Напишите программу, которая выводит случайное трехзначное число 
+//и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Рандомное число: {number}");
+// int a = number / 100;
+// int b = number % 10; 
+// number = a * 10 + b;
+// Console.WriteLine(number);
+
+
+//Задание 1
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// int number = new Random().Next(10, 100);
+// Console.WriteLine($"Рандомное число: {number}");
+// int a = number / 10;
+// int b = number % 10; 
+// if (a > b )
+// {
+//     Console.WriteLine(a);
+// }
+// else if(a < b)
+// {
+//     Console.WriteLine(b);
+// }
