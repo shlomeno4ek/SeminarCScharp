d98e12cda36042432443fa13c30aebc6ac2dc29e
Shloma Sergey <shlomasn@gmail.com>
2023-10-23 21:52:18 +0300

    Edded HomeTask4 and Seninar4

--- HomeTaskSeminar4/Program.cs
+++ HomeTaskSeminar4/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task 25: Напишите цикл, который принимает на вход два числа (A и B)
+//и возводит число A в натуральную степень B.
+
+// Console.Write("Введите целое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите степень числа: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"{a} в степени {b} = {Stepen(a, b)}");
+
+// int Stepen(int number, int stepen)
+// {
+//     int result = 1;
+
+//     for (int i = 0; i < stepen; i++)
+//     {
+//         result *= number;    
+//     }
+
+//     return result;
+// }
+
+
+//////////////////////////////////////////////////////////////////
+
+//Task 27: Напишите программу, которая принимает на вход число
+//и выдаёт сумму цифр в числе.
+
+// Console.Write("Введите целое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Сумма цифр числа {a} = {SumCifr(a)}");
+
+// int SumCifr (int number)
+// {
+//     int result = 0;
+
+//     while(number > 0)
+//     {
+//         result += number % 10;
+//         number /= 10;
+//     }
+
+//     return result;
+// }
+
+//////////////////////////////////////////////////////////////////
+
+//Task 29: Напишите программу, которая задаёт массив из 8 элементов
+//и выводит их на экран.
+
+int[] result = GetBinaryArray(8);
+
+Console.WriteLine($"[{String.Join("; ", result)}]");
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+// }
+
+int[] GetBinaryArray (int size)
+{
+    int[] mas = new int[size];
+
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = new Random().Next(100);
+    }
+
+    return mas;
+}
--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Методы: воз-т значения и void
+// тип метода ИмяМетода(параметр1б параметр2)
+
+using System.Runtime.ConstrainedExecution;
+using Internal;
+
+int Calculate(int first, int second, char sing)
+{
+    if (sing == '+')
+    {
+        return first + second;
+    }
+    else if (sing == '-')
+    {
+        return first - second;
+    }
+    else if (sing == '*')
+    {
+        return first * second;
+    }
+    else
+    {
+        Console.WriteLine("Ошибка! Введен не верный знак операции");
+        return 0;
+    }
+}
