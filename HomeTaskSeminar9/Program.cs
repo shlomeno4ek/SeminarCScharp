e6059702c3168d8a222104997d6698d0ed1fbbc5
Shloma Sergey <shlomasn@gmail.com>
2023-11-23 21:35:45 +0300

    eddit task1 and 2

--- HomeTaskSeminar9/Program.cs
+++ HomeTaskSeminar9/Program.cs
@@ -3,32 +3,34 @@
 // Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-// int n = 8;
 
-// printN(n);
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-// int printN(int n)
+// string printN(int n)
 // {
-//     Console.Write($"{n}, ");
-//     if (n == 1) return 1;
+//     if (n == 1) return n.ToString();
 
-//     return printN(n -1);
+//     return n + ", " + printN(n-1);
 // }
+// Console.Write(printN(n));
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
 // натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-// int m = 4;
-// int n = 8;
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
 // Console.Write(summNumber(m, n));
-// int summNumber(int start, int finish)
+// int summNumber(int start, int end)
 // {
-//     if (start == finish) return start;
+//     if (start == end) return start;
 
-//     return start + summNumber(start + 1, finish);
+//     return start + summNumber(start + 1, end);
 // }
 
 
@@ -37,14 +39,16 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-int m = 3;
-int n = 2;
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(functionAkkerman(m, n));
+// Console.Write(functionAkkerman(m, n));
 
-int functionAkkerman(int m, int n)
-{
-    if (m == 0) return (n + 1);
-    if (n == 0) return functionAkkerman(m -1, 1);
-    return functionAkkerman(m - 1, functionAkkerman(m, n -1));
-}
+// int functionAkkerman(int m, int n)
+// {
+//     if (m == 0) return (n + 1);
+//     if (n == 0) return functionAkkerman(m -1, 1);
+//     return functionAkkerman(m - 1, functionAkkerman(m, n -1));
+// }
